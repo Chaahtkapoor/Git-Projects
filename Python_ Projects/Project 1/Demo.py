260a6f3fcb9b0bfa455036ac9edb7c3aabc212a9
Chaaht Kapoor <68651448+Chaahtkapoor@users.noreply.github.com>
2020-07-26 22:56:58 +0530

    Initial commit

---
+++ Python_ Projects/Project 1/Demo.py
@@ -0,0 +1,52 @@
+n= int(input("Please Enter any Number: "))
+sum = 0
+
+for value in range(1, n + 1):
+    sum = sum + value
+
+average = sum / n
+print ("average of", n, "number is", average)
+#%%
+
+
+num= int(input("Please Enter any Number: "))
+
+factorial = 1
+if num < 0:
+    print("sorry, factorial does not exist")
+elif num == 0:
+    print("The factorial of 0 is 1")
+else:
+    for i in range(1, num +1):
+        factorial = factorial*i
+    print("the factorial of", num, " is", factorial) 
+ #%%
+num = int(input("Enter any number : "))
+if num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            print(num, "is NOT a prime number")
+            break
+    else:
+        print(num, "is a PRIME number")
+elif num == 0 or 1:
+    print(num, "is a neither prime NOR composite number")
+else:
+    print(num, "is NOT a prime number it is a COMPOSITE number")
+#%%
+n= int(input("Enter your number"))
+sum = 0
+for i in range (1, n+1):
+    sum = sum +(1/i)
+print ("The sum of series is", round(sum,2))    
+#%%
+import calendar
+yy= int(input("Enter year"))
+mm = int (input("Enter month"))
+print(calendar.month(yy,mm))
+#%%
+rows= int (input("Enter your number"))
+for i in range (1,rows+1):
+    for j in range (1, 1+i):
+        print (j, end='')
+    print("")    
